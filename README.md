81e22e0b178591a7063981dd88ec741e88fc6bf8
Joscha Legewie <jlegewie@Joschas-MBP.central.yale.internal>
2017-09-13 12:09:45 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Reveal.js plugin - d3js
+
+[Reveal.js](https://github.com/hakimel/reveal.js) plugin to embed d3.js visualizations in slides
+with support for transitions triggerd by normal slide navigation. d3.js visualizations are embed as
+iframe elements.
+
+<!-- [Check out the live demo](https://jlegewie.github.io/reveal.js-demos/anything-demo.html) -->
+
+
+## Installation
+
+1. Copy the file `d3js.js` into the plugin folder of your reveal.js presentation, i.e. `plugin/d3js`.
+
+2. Add the plugins to the dependencies in your presentation
+
+```javascript
+Reveal.initialize({
+	// ...
+	dependencies: [
+		// ... 
+		{ src: 'https://d3js.org/d3.v4.min.js' },
+		{ src: 'plugin/d3js/d3js.js' },
+		// ... 
+	]
+});
+```
+
+### Adding d3.js visualizations to slides
+
+To add a d3.js visualizations to your presentation, simply create a container element with the
+class `fig-container`, the attribute `data-fig-id` with the figure id, and `data-file` with the
+path to the html page with the d3.js visualizations. The container can be a `div` or `span` element
+in a slide or the `section` element to add the visualizations to the background.
+
+In the example, `index.html` contains the following code:
+
+```html
+<section>
+    <div class="fig-container"
+        data-fig-id="fig-collision-detection"
+        data-file="Collision-Detection.html"></div>
+</section>
+```
+
+### Adding transitions
+
+To add transitions, simply create two global variables in the html page with the d3.js
+visualizations:
+
+1. `_transitions` is an array with functions that for each transition.
+
+2. `_inverse_transitions` is an array with functions that revert each transition when user
+navigates back. `_inverse_transitions` can be set to `[]` if this functionality is not required.
+
+The plugin automatically looks at the number of elements in the array `_transitions` and creates
+corresponding fragments in the container slide.
+
+In the example, `Collision-Detection.html` adds two transitions:
+
+```javascript
+var _transitions = [],
+    _inverse_transitions = [];
+
+_transitions.push(
+  () => d3.selectAll("circle")
+ 			.transition().duration(1000)
+ 			.attr("r", function(d) { return d.radius * runif(0, 2); })
+);
+_inverse_transitions.push(
+  () => d3.selectAll("circle")
+  			.transition().duration(1000)
+  			.attr("r", function(d) { return d.radius; })
+);
+_transitions.push(
+  () => d3.selectAll("circle")
+  			.transition().duration(1500)
+  			.style("fill", "#33ff33")
+);
+_inverse_transitions.push(
+  () => d3.selectAll("circle")
+  			.transition().duration(1500)
+  			.style("fill", function(d, i) { return color(i % 3); })
+);
+```
+
+
+## License
+
+MIT licensed
+
+Copyright (C) 2017 Joscha Legewie
---
+++ d3js.js
@@ -0,0 +1,99 @@
+/**
+ * reveal.js plugin to integrate d3.js visualizations into presentations.
+ */
+var Reveald3js = window.Reveald3js || (function(){
+
+    function initialize(element, id, file, transitions) {
+        console.log("opening figure '"+ file + "'");
+
+        // get slide, slide background and container
+        var background = element.tagName == 'SECTION',
+            slide = background ? element : element.closest('section'),
+            idx = Reveal.getIndices(slide),
+            slide_background = background ? Reveal.getSlideBackground(idx.h, idx.v) : undefined;
+            container = background ? slide_background : element;
+
+        // embed html files as iframe
+        // allowfullscreen mozallowfullscreen webkitallowfullscreen style="width: 100%; height: 100%; max-height: 100%; max-width: 100%;"
+        var iframe = d3.select(container)
+            .append('iframe')
+            .attr('id', id)
+            .attr('sandbox', 'allow-popups allow-scripts allow-forms allow-same-origin')
+            .attr('src', file)
+            .attr('marginwidth', 0)
+            .attr('marginheight', 0)
+            .attr('scrolling', 'no')
+            .style('width', '100%')
+            .style('height', '100%')
+            .style('z-index', 1)
+            .node();
+
+        // iframe load event
+        iframe.onload = function() {
+            var fig = (iframe.contentWindow || iframe.contentDocument);
+            // add fragments to slide for figure transitions
+            var transitions_settings = parseInt(element.getAttribute('data-transitions'), 10),
+                transitions_file = !(fig._transitions) ? 0 : fig._transitions.length,
+                transitions = (transitions_settings || transitions_file);
+            if(transitions) {
+                var fragments = d3.select(slide)
+                    .selectAll('.fragment.fig-transition')
+                    .data((new Array(transitions)));
+                fragments.enter().append('span').attr('class', 'fragment fig-transition');
+                // set transition state
+                fig._transition_state = 0;
+            }
+            // add custom event listener to propatage key presses
+            fig.addEventListener('keydown', function(e) {
+                var event = new CustomEvent('iframe-keydown', { detail: e });
+                window.parent.document.dispatchEvent(event)
+            });
+        };
+    }
+
+    Reveal.addEventListener('ready', function( event ) {        
+        // Get all figure containers
+        var elements = document.querySelector('div.slides').querySelectorAll('.fig-container');
+        for (var j = 0; j < elements.length; j++ ) {
+            // get id and file attributes
+            var id = elements[j].getAttribute('data-fig-id'),
+                file = elements[j].getAttribute('data-file');
+            // load figure
+            initialize(elements[j], id, file);
+        }
+    });
+
+    // propagate keydown when focus is on iframe
+    // https://stackoverflow.com/a/41361761/2503795
+    window.document.addEventListener('iframe-keydown',
+        () => Reveal.triggerKey(event.detail.keyCode), false);
+
+    // figure transitions on fragment change
+    Reveal.addEventListener('fragmentshown', function(event) {
+        // only proceed if fragment has `fig-transition` class
+        if(!event.fragment.className.includes('fig-transition')) return;
+        // forward transition
+        var slide = event.fragment.closest('section'),
+            idx = Reveal.getIndices(slide),
+            iframe = slide.querySelector('iframe') ||
+                Reveal.getSlideBackground(idx.h, idx.v).querySelector('iframe'),
+            fig = (iframe.contentWindow || iframe.contentDocument);
+        fig._transitions[fig._transition_state]();
+        fig._transition_state = fig._transition_state + 1;
+    });
+
+    // figure inverse transitions on fragment change
+    Reveal.addEventListener('fragmenthidden', function(event) {
+        // only proceed if fragment has `fig-transition` class
+        if(!event.fragment.className.includes('fig-transition')) return;
+        // backward transition
+        var slide = event.fragment.closest('section'),
+            idx = Reveal.getIndices(slide),
+            iframe = slide.querySelector('iframe') ||
+                Reveal.getSlideBackground(idx.h, idx.v).querySelector('iframe'),
+            fig = (iframe.contentWindow || iframe.contentDocument);
+        fig._transition_state = fig._transition_state - 1;
+        (fig._inverse_transitions[fig._transition_state] || Function)();
+    });
+
+})();
